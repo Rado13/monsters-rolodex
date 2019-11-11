c6e4fc8d1d973883c4c2a681dc4fe6d44cc83cf1
Rado13 <dsadsa55@centrum.sk>
2019-11-11 15:04:07 +0100

    Updates

---
+++ precache-manifest.29fa1bccabbb1b804bf6eaa545cf02ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d717ace380e4b47d58aa7ffbc3a3f243",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "9e12cf64b4f99ca4c306",
+    "url": "/monsters/static/css/main.390aebdf.chunk.css"
+  },
+  {
+    "revision": "391cb0e4e182aa14b4b0",
+    "url": "/monsters/static/js/2.5430253d.chunk.js"
+  },
+  {
+    "revision": "9e12cf64b4f99ca4c306",
+    "url": "/monsters/static/js/main.09c93a3e.chunk.js"
+  },
+  {
+    "revision": "ed3573699ad6d0652769",
+    "url": "/monsters/static/js/runtime-main.5eba8463.js"
+  }
+]);
